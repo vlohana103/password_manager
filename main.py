19f8e6c60ad3a129557755d089f17a34bae1460b
Vikas <vlohana103@gmail.com>
2026-04-07 23:51:29 -0400

    added more functionality for update and delete, and worked on menu

--- main.py
+++ main.py
@@ -10,43 +10,87 @@ def create():
         password = input("Please input the password\n")
         password_manager[website_name] = (user, password)
         
-        ask = input("Would you like to add another log(press q to quit)?\n")
+        ask = input("Would you like to add another log(press q to return to main menu)?\n")
         if ask.lower() == "q":
             print("Thanks for using the Password Manager!")
             break
 
 ##### Read
 def read():
-    i = 0
     for website_name, (user, password) in password_manager.items():
-        print(f"Password Manager Log {i}: {website_name} -> {user}, {password}")
-        i += 1
+        print(f"Password Manager Log: {website_name} -> {user}, {password}")
+    
 
 ##### Update
+def update():
+    print(password_manager)
+    update_item = input("Which log would you like to update?\n")
+    for website_name, (user, password) in password_manager.items():
+        confirm = input(f"Are you sure you want to Update {update_item}? (Y/N)\n")
+        if confirm.lower() == "n":
+            print(f"{update_item} has not been updated")
+            break
+        elif confirm.lower() == "y":
+            print(f"Updating {update_item}.")
+            password_manager.update(update_item)
+            break
+        else:
+            print("Invalid Option.")        
 
 ##### Delete
 def delete():
-    pass
+    print(password_manager)
+    remove_item = input("Which log would you like to remove?\n")
+    for website_name, (user, password) in password_manager.items():
+        confirm = input(f"Are you sure you want to Delete {remove_item}? (Y/N)\n")
+        if confirm.lower() == "n":
+            print(f"{remove_item} has not been deleted")
+            break
+        elif confirm.lower() == "y":
+            print(f"Deleting {remove_item}.")
+            del password_manager[remove_item]
+            break
+        else:
+            print("Invalid Option.")
 
 
 print("Welcome to the Password Manager!")
+password_manager = {}
 
 while True:
-    menu_options = input("Press '1' to add a Password Log,\nPress '2' to view log\nPress '3' to exit\n")
-    password_manager = {}
+    use = input("Press '1' to use the Password Manager\nPress '2' to exit\n")
+    if use == '1':
+        ###
+        menu_options = input("Press '1' to add a Password Log,\nPress '2' to view log\nPress '3' to update log,\nPress '4' to delete a log\nPress '5' to exit\n")
 
-    # Create
-    if menu_options == '1':
-        create()
+        # Create
+        if menu_options == '1':
+            create()
 
-    # Read
-    elif menu_option == '2':
-        read()
+        # Read
+        elif menu_options == '2':
+            read()
 
-    elif menu_options == '3':
-        print("Thank you for using the Password Manager. Bye!")
+        # Update
+        elif menu_options == '3':
+            update()
+        
+        # Delete
+        elif menu_options == '4':
+            delete()
+
+        # Exit
+        elif menu_options == '5':    
+            print("Thank you for using the Password Manager. Bye!")
+            break
+        
+        # Invalid Input
+        else:
+            print("Invalid Input")
+
+
+    elif use == '2':
+        print("Exiting ...")
         break
-
-
     else:
-        print("Invalid Input")
+        print("Invalid Input. Please enter '1' or '2'")
